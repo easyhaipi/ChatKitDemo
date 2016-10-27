ca8762659a19f26eb82e66cc1adc5bf577085eef
easyhaipi <easy_qingsong@163.com>
2016-10-27 09:57:11 +0800

    Initial Commit

---
+++ ChatKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatKitDemo
+//
+//  Created by taobaichi on 2016/10/27.
+//  Copyright © 2016年 taobaichi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatKitDemo
+//
+//  Created by taobaichi on 2016/10/27.
+//  Copyright © 2016年 taobaichi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
